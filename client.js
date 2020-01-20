33814e4f6d0a82c0685fbd23b41216741d0b1bd2
steffan <potentialsteffan@gmail.com>
2020-01-21 00:12:53 +0800

    增加可读性牌值并排序

--- client.js
+++ client.js
@@ -8,6 +8,7 @@ const CMD_ID = [
 ]
 const net = require('net');
 const readlineSync = require('readline-sync');
+// const {DZCardCheck} = require('./DZCardCheck');
 
 const socket = new net.Socket({});
 
@@ -60,7 +61,10 @@ function decode(buffer) {
     // }
 }
 function jiaoFen(body) {
-    console.log('发牌完毕,你的座位号为->', body.serverSeat, '你的手牌->', body.cards);
+    let _cards = body.cards.sort((a,b)=>{
+        return getCardValue(a) - getCardValue(b)
+    })
+    console.log('发牌完毕,你的座位号为->', body.serverSeat, '你的手牌->', revert0x2h(_cards));
     console.log('开始叫分（输入1-3）：')
     const _score =  input2Cmd()
     console.log('叫了'+_score+"分")
@@ -69,6 +73,15 @@ function jiaoFen(body) {
 function input2Cmd() {
     return readlineSync.question();
 
+}
+/**转换为玩家认识的符号 */
+function revert0x2h(cardsArr){
+    let _res = [];
+    for(let i =0,len = cardsArr.length;i<len;i++){
+        let _card = cardsArr[i];
+        _res.push(getCardName(_card));
+    }
+    return _res;
 }
 function checkComplete(buffer) {
     if (buffer.length < 8) {
@@ -80,6 +93,55 @@ function checkComplete(buffer) {
 function reqSendCards() {
     socket.write(encode({ cmd: CMD_ID[0], data: { "msg": 'test' } }));
 }
+getCardName = function (cardSerialNo) {
+    var resStr = -1;
+    cardSerialNo = Number(cardSerialNo);
+    resStr = cardSerialNo % 16;
+    //大小王
+    if (resStr == 14) {
+        resStr = "sJoker";
+    }
+    else if (resStr == 15) {
+        resStr = "bJoker";
+    }
+    //A,2
+    else if (resStr == 1) {
+        resStr = "A";
+    }
+    else if (resStr == 2) {
+        resStr = "2";
+    }
+    else if(resStr == 11){
+        resStr = 'J'
+    }
+    else if(resStr == 12){
+        resStr = 'Q'
+    }
+    else if(resStr == 13){
+        resStr = 'K'
+    }
+    return resStr+"";
+};
+getCardValue = function (cardSerialNo) {
+    var resNum = -1;
+    cardSerialNo = Number(cardSerialNo);
+    resNum = cardSerialNo % 16;
+    //大小王
+    if (resNum == 14) {
+        resNum = 16;
+    }
+    else if (resNum == 15) {
+        resNum = 17;
+    }
+    //A,2
+    else if (resNum == 1) {
+        resNum = 14;
+    }
+    else if (resNum == 2) {
+        resNum = 15;
+    }
+    return resNum;
+};
 function startGame() {
     reqSendCards();
 }
